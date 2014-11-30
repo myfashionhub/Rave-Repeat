22defc27f5ddb51679caf75268d5abe575e3fb01
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-11-30 14:22:02 -0500

    Adjust seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
 require 'csv'
 
 CSV.foreach("#{Rails.root}/db/festivals.csv") do |row|
-  Festival.create(name: row[0], location: row[1], start_date: row[2], end_date: row[3], playlist: row[4], playlist_scid: row[5], status: row[6], lineup: row[7].split(','))
+  existing_festival = Festival.find_by(playlist: row[4])
+  existing_festival.update(
+    name: row[0],
+    location: row[1],
+    start_date: row[2],
+    end_date: row[3],
+    playlist: row[4],
+    playlist_scid: row[5],
+    status: row[6],
+    lineup: row[7].split(',')
+  )
+
+  Festival.find_or_create_by(
+    name: row[0],
+    location: row[1],
+    start_date: row[2],
+    end_date: row[3],
+    playlist: row[4],
+    playlist_scid: row[5],
+    status: row[6],
+    lineup: row[7].split(',')
+  )
+
 end
 
 # Initial heroku db seed
