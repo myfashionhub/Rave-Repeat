1abc1ffc7c19418eba35c20fdd92f82ba05ed556
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-12-01 23:28:31 -0500

    Refactor some partials

--- app/controllers/ravers_controller.rb
+++ app/controllers/ravers_controller.rb
@@ -11,27 +11,22 @@ class RaversController < ApplicationController
   end
 
   def trips
-    all_trips = Trip.where(raver_id: current_raver.id)
     if params[:past]
-      trips = all_trips.select do |trip|
-        puts trip.start_date.class
-        puts trip.start_date < Date.today
-        trip.start_date < Date.today
-      end
+      trips = Trip.where(raver_id: current_raver.id)
+                .where('start_date < ?', Date.today)
     elsif params[:upcoming]
-      trips = all_trips.select do |trip|
-        puts trip.start_date >= Date.today
-        trip.start_date >= Date.today
-      end
+      trips = Trip.where(raver_id: current_raver.id)
+                .where('start_date >= ?', Date.today)
     end
 
     trip_info = trips.map do |trip|
       festival = Festival.find(trip.festival_id)
       #flight   = Flight.find_by(trip_id: trip.id)
       puts trip.start_date
+      puts trip.start_date.class
       { festival: festival.name,
-        start_date: trip.start_date,
-        end_date: trip.end_date,
+        start_date: display_date(trip.start_date),
+        end_date: display_date(trip.end_date),
         from_airport: trip.from_airport,
         to_airport: trip.to_airport,
         lineup: trip.lineup || 'Not specified',
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,6 @@
 require 'csv'
 
 CSV.foreach("#{Rails.root}/db/festivals.csv") do |row|
-  existing_festival = Festival.find_by(playlist: row[4])
-  existing_festival.update(
-    name: row[0],
-    location: row[1],
-    start_date: row[2],
-    end_date: row[3],
-    playlist: row[4],
-    playlist_scid: row[5],
-    status: row[6],
-    lineup: row[7].split(',')
-  )
-
   Festival.find_or_create_by(
     name: row[0],
     location: row[1],
@@ -24,17 +12,30 @@ CSV.foreach("#{Rails.root}/db/festivals.csv") do |row|
     lineup: row[7].split(',')
   )
 
+  existing_festival = Festival.find_by(playlist: row[4])
+  unless existing_festival.nil?
+    existing_festival.update(
+      name: row[0],
+      location: row[1],
+      start_date: row[2],
+      end_date: row[3],
+      playlist: row[4],
+      playlist_scid: row[5],
+      status: row[6],
+      lineup: row[7].split(',')
+    )
+  end
 end
 
 # Initial heroku db seed
-# names = ['ploxiln', 'Paul H1am', 'Daul Be', 'Nathan Lee', 'Jason_on_the_block', 'Clara T', 'Kaylee182', 'EDM Sauce', 'Becky', 'Skrillex4life', 'Il0veCalvinHarris', 'born2rave', 'Bass_drop002', 'Trance_or_die', 'haveuseenmolly']
+names = ['ploxiln', 'Paul H1am', 'Daul Be', 'Nathan Lee', 'Jason_on_the_block', 'Clara T', 'Kaylee182', 'EDM Sauce', 'Becky', 'Skrillex4life', 'Il0veCalvinHarris', 'born2rave', 'Bass_drop002', 'Trance_or_die', 'haveuseenmolly']
 
-# ravers = []
-# names.each do |name|
-#   ravers.push(Raver.create(name: name))
-# end
+ravers = []
+names.each do |name|
+  ravers.push(Raver.create(name: name))
+end
 
-# ravers.each do |raver|
-#   rand = (6..10).to_a.sample
-#   Trip.create(raver_id: raver.id, festival_id: rand)
-# end
+ravers.each do |raver|
+  rand = (1..10).to_a.sample
+  Trip.create(raver_id: raver.id, festival_id: rand)
+end
