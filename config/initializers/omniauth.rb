24182b42cf254beb045c8939f61565a73a08bf1d
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2017-11-10 11:18:33 -0500

    Add Facebook callback URL

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['RR_TWITTER_KEY'], ENV['RR_TWITTER_SECRET']
-  provider :facebook, ENV['RR_FACEBOOK_ID'], ENV['RR_FACEBOOK_SECRET']
+  provider :twitter,
+    ENV['RR_TWITTER_KEY'], ENV['RR_TWITTER_SECRET']
+  provider :facebook,
+    ENV['RR_FACEBOOK_ID'], ENV['RR_FACEBOOK_SECRET'],
+    callback_url: "#{ENV['RR_HOST']}/auth/facebook/callback"
 end
 
